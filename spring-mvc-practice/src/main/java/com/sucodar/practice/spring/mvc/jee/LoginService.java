27ebecd8d76974525f1c56aedbe551587817cd99
webdev0505 <webdev0505@gmail.com>
2019-05-15 09:40:50 +0530

    Old project removed

--- spring-mvc-practice/src/main/java/com/sucodar/practice/spring/mvc/jee/LoginService.java
+++
@@ -1,8 +0,0 @@
-package com.sucodar.practice.spring.mvc.jee;
-
-public class LoginService {
-
-	public boolean validateUser(String user, String password) {
-		return user.equalsIgnoreCase("test") && password.equals("test");
-	}
-}
